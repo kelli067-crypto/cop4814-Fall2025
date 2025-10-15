0845541ea608c18f2a656dd0650656952ac7ab8d
Kenneth Ellis <kelli067@fiu.edu>
2025-10-15 15:12:19 -0400

    10-15 API

---
+++ astronauts.py
@@ -0,0 +1,36 @@
+import requests # requests is the python package for APT request
+import main_functions
+
+#API url
+url = "http://api.open-notify.org/astros.json"
+
+#Make the API request
+response = requests.get(url).json()
+
+#Serialize the result to a JSON document
+main_functions.save_to_file(response,"astronauts.json") #make a file to save localy
+
+#Deserialize the recently create JSON document
+astronauts = main_functions.read_from_file("astronauts.json")
+
+#What is the type of the object deserialized?
+print(type(astronauts))
+
+#What are its keys?
+print(astronauts.keys())
+#Access some of their values passing their keys
+print(astronauts["people"])
+print(astronauts["number"])
+print(astronauts["message"])
+
+#What are the names of the astronauts?
+print(astronauts["people"], ["name"])
+#Print their names using a for loop
+for i in astronauts["people"]:
+    print((i)["name"])
+#Sort their names
+for j in sorted(astronauts['people'],key = lambda x : x["name"]):
+    print(j["name"])
+
+#for j in sorted(astronautsDict['people'],key = lambda x : x["name"]):
+    #print(j["name"])
